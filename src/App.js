93bdec2e7e1a341948e8b91cefe13051bd6e0bb3
Desire Amagwula <dezamagwula@gmail.com>
2022-03-12 14:56:48 -0600

    This is the first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import QRCode from 'qrcode';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ({ text }) => {
+  const [src, setSrc] = useState('');
+
+  useEffect(() => {
+    QRCode.toDataURL(text).then((data) => {
+      setSrc(data);
+    });
+
+  }, [])
+    return (
+    <div>
+      <img src={src}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
